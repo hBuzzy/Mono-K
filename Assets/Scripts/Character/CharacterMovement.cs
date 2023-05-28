50786417fe073c39e401496e396538286ed3c9e9
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-05-28 16:29:56 +0300

    Refactoring

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -1,19 +1,32 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class CharacterMovement : MonoBehaviour
-{[Header("Movement")]
-    [SerializeField] private float _moveSpeed;
-    [SerializeField] private float _acceleration;
-    [SerializeField] private float _deceleration;
+{
+    [FormerlySerializedAs("_moveSpeed")]
+    [Header("Movement")]
+    [SerializeField, Range(0f, 20f)] private float _maxMoveSpeed;
+    [SerializeField, Range(0f, 100f)] private float _acceleration;
+    [SerializeField, Range(0f, 100f)] private float _deceleration;
+    [SerializeField, Range(0f, 100f)] private float _turnSpeed;
 
     [Header("Jump")]
     [SerializeField] private float _jumpForce;
 
+    [SerializeField, Range(0f, 20f)] private float _maxJumpHeight;
+    [SerializeField, Range(0f, 100f)] private float _airAcceleration;
+    [SerializeField, Range(0f, 100f)] private float _airDeceleration;
+    [SerializeField, Range(0f, 100f)] private float _airTurnSpeed;
+    
     [SerializeField] private LayerMask _groundMask;
 
     private PlayerInputActions _playerInput;
     private Rigidbody2D _rigidbody;
+
+    private float _speedChange;
+    private Vector2 _velocity;
+    private Vector2 _desiredVelocity;
 
     private void Awake()
     {
@@ -39,7 +52,10 @@ public class CharacterMovement : MonoBehaviour
     private void Update()
     {
         Vector2 moveDirection = _playerInput.Character.Move.ReadValue<Vector2>();
-        Move(moveDirection);
+        float friction = 0;
+        _desiredVelocity = new Vector2(moveDirection.x, 0f) * Mathf.Max(_maxMoveSpeed - friction, 0f);
+        
+        Run(moveDirection);
     }
     
     private void FixedUpdate()
@@ -55,14 +71,37 @@ public class CharacterMovement : MonoBehaviour
         _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
     }
 
-    private void Move(Vector2 direction)
+    private void Run(Vector2 direction)
     {
-        _rigidbody.velocity = new Vector2(direction.x * _moveSpeed, _rigidbody.velocity.y);
+        //_rigidbody.velocity = new Vector2(direction.x * _moveSpeed, _rigidbody.velocity.y);
         //transform.position += new Vector3(direction.x, 0, direction.y) * (_moveSpeed * Time.deltaTime);
+
+        float acceleration = IsGrounded() ? _acceleration : _airAcceleration;
+        float deceleration = IsGrounded() ? _deceleration : _airDeceleration;
+        float turnSpeed = IsGrounded() ? _turnSpeed : _airTurnSpeed;
+
+        if (direction.x == 0) //TODO: Refactoring 
+        {
+            _speedChange = deceleration * Time.deltaTime;
+        }
+        else
+        {
+            if (Mathf.Sign(direction.x) != Mathf.Sign(_velocity.x))
+            {
+                _speedChange = turnSpeed * Time.deltaTime;
+            }
+            else
+            {
+                _speedChange = acceleration * Time.deltaTime;
+            }
+        }
+
+        _velocity.x = Mathf.MoveTowards(_velocity.x, _desiredVelocity.x, _speedChange);
+        _rigidbody.velocity = _velocity;
     }
 
     private bool IsGrounded()
     {
-        return false;
+        return true;
     }
 }
