df44674f0592d91c5836ffb770c81cea0b9f4a08
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-05-29 13:40:13 +0300

    Improve movement with gravity calculations

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -5,38 +5,35 @@ using UnityEngine.Serialization;
 
 public class CharacterMovement : MonoBehaviour
 {
-    [FormerlySerializedAs("_moveSpeed")]
-    [Header("Movement")]
+    [Header("Speeds")]
     [SerializeField, Range(0f, 20f)] private float _maxMoveSpeed;
-    [SerializeField, Range(0f, 100f)] private float _acceleration;
-    [SerializeField, Range(0f, 100f)] private float _deceleration;
     [SerializeField, Range(0f, 100f)] private float _turnSpeed;
-
-    [Header("Jump")]
-    [SerializeField] private float _jumpForce;
-
-    [SerializeField, Range(0f, 20f)] private float _maxJumpHeight;
-    [SerializeField, Range(0f, 100f)] private float _airAcceleration;
-    [SerializeField, Range(0f, 100f)] private float _airDeceleration;
     [SerializeField, Range(0f, 100f)] private float _airTurnSpeed;
     
-    [SerializeField] private LayerMask _groundMask;
-
+    [Header("Accelerations")]
+    [SerializeField, Range(0f, 100f)] private float _acceleration;
+    [SerializeField, Range(0f, 100f)] private float _deceleration;
+    [SerializeField, Range(0f, 100f)] private float _airAcceleration;
+    [SerializeField, Range(0f, 100f)] private float _airDeceleration;
+    
     private PlayerInputActions _playerInput;
+    private Grounder _grounder;
     private Rigidbody2D _rigidbody;
 
     private float _speedChange;
     private Vector2 _velocity;
     private Vector2 _desiredVelocity;
+    private float directionX;
+    private bool isGrounded;
 
     private void Awake()
     {
         _playerInput = new PlayerInputActions();
-        _playerInput.Character.Jump.performed += context => Jump();
     }
 
     private void Start()
     {
+        _grounder = GetComponent<Grounder>();
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -52,54 +49,35 @@ public class CharacterMovement : MonoBehaviour
     
     private void Update()
     {
-        Vector2 moveDirection = _playerInput.Character.Move.ReadValue<Vector2>();
+        directionX = _playerInput.Character.Move.ReadValue<Vector2>().x;
         float friction = 0;
-        _desiredVelocity = new Vector2(moveDirection.x, 0f) * Mathf.Max(_maxMoveSpeed - friction, 0f);
-        
-        Run(moveDirection);
+        _desiredVelocity = new Vector2(directionX, 0f) * Mathf.Max(_maxMoveSpeed - friction, 0f);
     }
     
     private void FixedUpdate()
     {
-        if (_rigidbody.velocity.y < 0)
-        {
-            _rigidbody.velocity += Vector2.up * (Physics2D.gravity.y * 0.7f * Time.deltaTime);
-        }
+        isGrounded = _grounder.IsGrounded;
+        _velocity = _rigidbody.velocity;
+
+        Move();
     }
 
-    private void Jump()
-    {
-        // //_rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-        // var jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * _rigidbody.gravityScale * _maxJumpHeight);
-        //
-        // if (_velocity.y > 0f) {
-        //     jumpSpeed = Mathf.Max(jumpSpeed - _velocity.y, 0f);
-        // }
-        // else if (_velocity.y < 0f) {
-        //     jumpSpeed += Mathf.Abs(_rigidbody.velocity.y);
-        // }
-        //
-        // _velocity.y += jumpSpeed;
-        //
-        // Debug.Log("jumped");
-    }
-
-    private void Run(Vector2 direction)
+    private void Move()
     {
         //_rigidbody.velocity = new Vector2(direction.x * _moveSpeed, _rigidbody.velocity.y);
         //transform.position += new Vector3(direction.x, 0, direction.y) * (_moveSpeed * Time.deltaTime);
 
-        float acceleration = IsGrounded() ? _acceleration : _airAcceleration;
-        float deceleration = IsGrounded() ? _deceleration : _airDeceleration;
-        float turnSpeed = IsGrounded() ? _turnSpeed : _airTurnSpeed;
+        float acceleration = isGrounded ? _acceleration : _airAcceleration;
+        float deceleration = isGrounded ? _deceleration : _airDeceleration;
+        float turnSpeed = isGrounded ? _turnSpeed : _airTurnSpeed;
 
-        if (direction.x == 0) //TODO: Refactoring 
+        if (directionX == 0) //TODO: Refactoring 
         {
             _speedChange = deceleration * Time.deltaTime;
         }
         else
         {
-            if (Mathf.Sign(direction.x) != Mathf.Sign(_velocity.x))
+            if (Mathf.Sign(directionX) != Mathf.Sign(_velocity.x))
             {
                 _speedChange = turnSpeed * Time.deltaTime;
             }
@@ -111,10 +89,5 @@ public class CharacterMovement : MonoBehaviour
 
         _velocity.x = Mathf.MoveTowards(_velocity.x, _desiredVelocity.x, _speedChange);
         _rigidbody.velocity = _velocity;
-    }
-
-    private bool IsGrounded()
-    {
-        return true;
     }
 }
