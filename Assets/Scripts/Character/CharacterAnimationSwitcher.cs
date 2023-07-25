19e29647b4537ef6411de4f68e54ed5f2cc1565b
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-07-25 17:02:36 +0300

    Refactoring , Added dash preparation

--- Assets/Scripts/Character/CharacterAnimationSwitcher.cs
+++ Assets/Scripts/Character/CharacterAnimationSwitcher.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class CharacterAnimationSwitcher : MonoBehaviour
 {
     [SerializeField] private CharacterStates _states;
-    
+
     private readonly int _idle = Animator.StringToHash("Idle");
     private readonly int _jumpStart= Animator.StringToHash("JumpStart");
     private readonly int _jump = Animator.StringToHash("Jump");
-    private readonly int _fall = Animator.StringToHash("Fall");
     private readonly int _jumpEnd = Animator.StringToHash("JumpEnd");
+    private readonly int _fall = Animator.StringToHash("Fall");
     private readonly int _walk = Animator.StringToHash("Walk");
+    private readonly int _dashPreparation = Animator.StringToHash("DashPreparation");
     private readonly int _dash = Animator.StringToHash("Dash");
     private readonly int _slide = Animator.StringToHash("Slide");
     private readonly int _hurt = Animator.StringToHash("Hurt");
 
+    private readonly int _transitionDuration;
+    
     private Animator _animator;
     
     private bool _isJumping;
@@ -37,10 +40,8 @@ public class CharacterAnimationSwitcher : MonoBehaviour
     private void AnimateState(CharacterStates.States state)
     {
         var stateAnimation = GetStateAnimation(state);
-        
-        //ResetVariables(state);
-        
-        _animator.CrossFade(stateAnimation, 0, 0);
+
+        _animator.CrossFade(stateAnimation, _transitionDuration, 0);
     }
     
     private int GetStateAnimation(CharacterStates.States state)
@@ -53,6 +54,11 @@ public class CharacterAnimationSwitcher : MonoBehaviour
         if (state == CharacterStates.States.Slide)
         {
             return _slide;
+        }
+
+        if (state == CharacterStates.States.DashPreparation)
+        {
+            return _dashPreparation;
         }
         
         if (state == CharacterStates.States.Dash)
@@ -77,10 +83,5 @@ public class CharacterAnimationSwitcher : MonoBehaviour
         }
         
         return _idle;
-    }
-
-    private void ResetVariables(int newState)
-    {
-
     }
 }
