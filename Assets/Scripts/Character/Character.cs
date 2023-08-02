fb0e7fd611fbe0a0f832806dcad2ef87fe02027b
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-08-02 21:22:46 +0300

    Refactoring

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using States = CharacterStates.States;
 
 [RequireComponent(typeof(CharacterStates))]
 [RequireComponent(typeof(Rigidbody2D))]
 
 public class Character : MonoBehaviour
 {
-    [SerializeField] private CharacterGround _groundDetector; //TODO: Rename
-    [SerializeField] private CharacterWallChecker _wallDetector;//TODO: Rename
-    [SerializeField] private Transform _respawnPoint;
+    [SerializeField] private CharacterGroundDetector _groundDetector;
+    [SerializeField] private CharacterWallDetector _wallDetector;
+    [SerializeField] private Transform _respawnPoint; //TODO Make it
     [SerializeField] private NightSwitcher _nightSwitcher;
     [SerializeField] private Light2D _light;
     [SerializeField] private TrailRenderer _trail;
@@ -16,20 +17,23 @@ public class Character : MonoBehaviour
     private CharacterStates _states;
     private PlayerInputActions _playerInput;
     private Rigidbody2D _rigidbody;
+
+    private States _currentState;
     
     private float _inputX;
     private float _facingDirectionX = 1f;
     
     private bool _canMove;
 
+    public Vector2 Velocity => _rigidbody.velocity;
+    
     public float InputX => _inputX;
     public float FacingDirectionX => _facingDirectionX;
+    public float GravityScale => _rigidbody.gravityScale;
     
     public bool CanMove => _canMove;
-    public bool IsTouchingWall => _wallDetector.IsTouchingWall;//TODO: Refactor checkers
-    public bool IsGrounded => _groundDetector.GetOnGround();
-    public Vector2 Velocity => _rigidbody.velocity;
-    public float GravityScale => _rigidbody.gravityScale;
+    public bool IsTouchingWall => _wallDetector.IsTouchingWall;
+    public bool IsGrounded => _groundDetector.IsGrounded;
 
     private void Awake()
     {
@@ -42,14 +46,14 @@ public class Character : MonoBehaviour
     {
         _playerInput.Character.Enable();
         _nightSwitcher.NightStarted += NightStarted;
-        _states.StateChanged += UpdateMoveState;
+        _states.StateChanged += OnStateChanged;
     }
 
     private void OnDisable()
     {
         _playerInput.Character.Disable();
         _nightSwitcher.NightStarted -= NightStarted;
-        _states.StateChanged -= UpdateMoveState;
+        _states.StateChanged -= OnStateChanged;
     }
 
     private void Update()
@@ -64,7 +68,7 @@ public class Character : MonoBehaviour
 
     public void SetVelocity(Vector2 velocity)
     {
-        if (_canMove)
+        if (_canMove || _currentState == States.Dash)
         {
             _rigidbody.velocity = velocity;
         }
@@ -77,6 +81,22 @@ public class Character : MonoBehaviour
             _rigidbody.gravityScale = gravityScale;
         }
     }
+
+    private void UpdateMoveAbility()
+    {
+        if (_currentState == CharacterStates.States.Dash ||
+            _currentState == CharacterStates.States.Grab ||
+            _currentState == CharacterStates.States.DashPreparation)
+        {
+            _rigidbody.gravityScale = 0f;
+            _rigidbody.velocity = Vector2.zero;
+            _canMove = false;
+        }
+        else
+        {
+            _canMove = true;
+        }
+    }
     
     private void NightStarted()
     {
@@ -86,15 +106,9 @@ public class Character : MonoBehaviour
         _trail.enabled = true;
     }
 
-    private void UpdateMoveState(CharacterStates.States state)//TODO:Rename
+    private void OnStateChanged(States state)//TODO:Rename
     {
-        if (state == CharacterStates.States.Dash || state == CharacterStates.States.Grab || state == CharacterStates.States.DashPreparation)
-        {
-            _canMove = false;
-        }
-        else
-        {
-            _canMove = true;
-        }
+        _currentState = state;
+        UpdateMoveAbility();
     }
 }
