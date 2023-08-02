f7431a3eebe42c71e187c02cdd800398640a1920
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-08-02 15:32:56 +0300

    Renamed collisions detectors

--- Assets/Scripts/Character/CharacterGroundDetector.cs
+++ Assets/Scripts/Character/CharacterGroundDetector.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class CharacterGround : MonoBehaviour
+public class CharacterGroundDetector : MonoBehaviour
 {
-    private bool _onGround;
+    private bool _isGrounded;
 
+    public bool IsGrounded => _isGrounded;
+    
     [FormerlySerializedAs("groundLength")]
     [Header("Collider Settings")]
     [SerializeField][Tooltip("Length of the ground-checking collider")] private float _groundLength = 0.95f;
@@ -16,16 +18,13 @@ public class CharacterGround : MonoBehaviour
 
     private void Update() {
         //Determine if the player is stood on objects on the ground layer, using a pair of raycasts
-        _onGround = Physics2D.Raycast(transform.position + _colliderOffset, Vector2.down, _groundLength, groundLayer) || Physics2D.Raycast(transform.position - _colliderOffset, Vector2.down, _groundLength, groundLayer);
+        _isGrounded = Physics2D.Raycast(transform.position + _colliderOffset, Vector2.down, _groundLength, groundLayer) || Physics2D.Raycast(transform.position - _colliderOffset, Vector2.down, _groundLength, groundLayer);
     }
 
     private void OnDrawGizmos() {
         //Draw the ground colliders on screen for debug purposes
-        if (_onGround) { Gizmos.color = Color.green; } else { Gizmos.color = Color.red; }
+        if (_isGrounded) { Gizmos.color = Color.green; } else { Gizmos.color = Color.red; }
         Gizmos.DrawLine(transform.position + _colliderOffset, transform.position + _colliderOffset + Vector3.down * _groundLength);
         Gizmos.DrawLine(transform.position - _colliderOffset, transform.position - _colliderOffset + Vector3.down * _groundLength);
     }
-
-    //Send ground detection to other scripts
-    public bool GetOnGround() { return _onGround; }
 }
--- Assets/Scripts/Character/CharacterWallDetector.cs
+++ Assets/Scripts/Character/CharacterWallDetector.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class CharacterWallChecker : MonoBehaviour
+public class CharacterWallDetector : MonoBehaviour
 {
     [SerializeField] private LayerMask _wallMask;
     
