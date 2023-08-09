03dd09307d4e4293dd3d77ccbcfafa4520991d61
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-08-09 20:24:55 +0300

    Initial commit

---
+++ Assets/Scripts/Effects/Outline.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Outline : MonoBehaviour//TODO: Change char material instead of global one
+{
+    [SerializeField] private float _thickness;
+    [SerializeField] private Material _material;
+    
+    private readonly int _outlineThickness = Shader.PropertyToID("_OutlineThickness");
+
+    public void Show()
+    {
+        _material.SetFloat(_outlineThickness, _thickness);
+    }
+
+    public void Hide()
+    {
+        _material.SetFloat(_outlineThickness, 0f);
+    }
+}
