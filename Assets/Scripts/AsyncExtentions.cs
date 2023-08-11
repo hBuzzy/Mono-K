b6c63f81a3c2b982dd122d373a1d133d5d1548b7
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-08-12 01:48:35 +0300

    Initial commit

---
+++ Assets/Scripts/AsyncExtentions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public abstract class AsyncExtensions
+{
+   public static async Task WaitForSeconds(float seconds)
+   {
+      await Task.Delay(TimeSpan.FromSeconds(seconds));
+   }   
+}
