361beaf47a8e40f8de841e2c58dedea66015340d
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-08-20 18:50:23 +0300

    Added close logic

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -3,12 +3,25 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    [Header("Points")]
     [SerializeField] private Transform _openedPoint;
-    [SerializeField] private AudioSource _openSound;
+    [SerializeField] private Transform _closePoint;
     
+    [Header("Settings")]
+    [SerializeField] private AudioSource _openSound;
+    [SerializeField] private float _moveDuration;
+
+    private Vector3 _currentPosition;
+
     public void Open()
     {
-        transform.DOMove(_openedPoint.position, 3f);
+        transform.DOMove(_openedPoint.position, _moveDuration);
+        _openSound.PlayOneShot(_openSound.clip);
+    }
+
+    public void Close()
+    {
+        transform.DOMove(_openedPoint.position, _moveDuration);
         _openSound.PlayOneShot(_openSound.clip);
     }
 }
