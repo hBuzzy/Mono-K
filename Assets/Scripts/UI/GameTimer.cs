6c47e9f269296c642eee7a5a7686d8bb02c72084
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-08-22 23:52:39 +0300

    Initial commit

---
+++ Assets/Scripts/UI/GameTimer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(TimerView))]
+
+public class GameTimer : MonoBehaviour //TODO: refactor connection betwen model and view
+{
+    private float _elapsedTime;
+    private bool _isActive;
+
+    private TimerView _view;
+
+    private void Start()
+    {
+        _view = GetComponent<TimerView>();
+    }
+
+    private void OnEnable()
+    {
+        //TODO: Subscribe to night started
+        //TODO: Subscribe to game (reached end pouint) ended
+    }
+
+    private void OnDisable()
+    {
+        //throw new NotImplementedException();
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        _view.Render(_elapsedTime);
+    }
+
+    private void OnStart()
+    {
+        
+    }
+
+    private void OnPause()
+    {
+        
+    }
+
+    private void OnStop()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/TimerView.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TimerView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _timeText;
+
+    private const int SecondsInMinute = 60;
+
+    public void Render(float time)
+    {
+        int seconds = Mathf.FloorToInt(time % SecondsInMinute);
+        int minutes = Mathf.FloorToInt(time / SecondsInMinute);
+        
+        _timeText.text = $"{minutes:00}:{seconds:00}";
+    }
+}
