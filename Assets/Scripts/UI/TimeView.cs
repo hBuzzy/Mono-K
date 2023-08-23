aad0c0f50ace682073db37c4681e13cb38807201
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-08-23 03:38:23 +0300

    Renamed

---
+++ Assets/Scripts/UI/TimeView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class TimeView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _timeText;
+
+    public void Render(float time)
+    {
+        TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+        
+        string hours = timeSpan.Hours == 0 ? "" : $"{timeSpan.Hours}:";
+        
+        _timeText.text = $"{hours}{timeSpan.Minutes:00}:{timeSpan.Seconds:00}";
+    }
+}
