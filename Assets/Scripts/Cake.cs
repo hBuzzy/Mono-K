260d1c884686c4f956f1ba962033f17d031349b1
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-08-23 03:39:32 +0300

    Improved disapear logic

--- Assets/Scripts/Cake.cs
+++ Assets/Scripts/Cake.cs
@@ -1,31 +1,50 @@
 using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+
 public class Cake : MonoBehaviour
 {
-    [SerializeField] private AudioSource _takeSound;
-    [SerializeField] private Transform _cakeImage;
-    [SerializeField, Range(0f, 0.5f)] private float _waitBeforeDisappear;
+    [SerializeField] private AudioSource _takingSound;
+
+    private const float AnimationTransitionDuration = 0.1f;
+    
+    private readonly int _disappear = Animator.StringToHash("Disappear");
+
+    private Animator _animator;
+    private bool _isDisappearFinished;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out PieBasket pieBasket))
         {
             pieBasket.AddPie();
-            StartCoroutine(Take());
+            StartCoroutine(Disappear());
         }
     }
 
-    private IEnumerator Take()
+    private void OnDisappearFinished()
     {
-        _takeSound.PlayOneShot(_takeSound.clip);
+        _isDisappearFinished = true;
+    }
 
-        yield return new WaitForSeconds(_waitBeforeDisappear);
+    private IEnumerator Disappear()
+    {
+        _takingSound.PlayOneShot(_takingSound.clip);
+
+        _isDisappearFinished = false;
+        _animator.CrossFade(_disappear, AnimationTransitionDuration);
+
+        while (_takingSound.isPlaying || _isDisappearFinished == false)
+        {
+            yield return null;
+        }
         
-        _cakeImage.gameObject.SetActive(false);
-
-        yield return new WaitForSeconds(_takeSound.clip.length);
-
         gameObject.SetActive(false);
     }
 }
