d8864e451da0acd07debe441defc27f09d99e064
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-11-08 23:02:24 +0300

    Update CharacterEffects.cs

--- Assets/Scripts/Character/CharacterEffects.cs
+++ Assets/Scripts/Character/CharacterEffects.cs
@@ -1,142 +1,96 @@
-using System.Collections;
-using Cinemachine;
-using DG.Tweening;
 using UnityEngine;
+using States = CharacterData.States;
+
+[RequireComponent(typeof(AudioSource))]
 
 public class CharacterEffects : MonoBehaviour
 {
-    [Header("Components")]
-    [SerializeField] private Camera _cameraMain;
-    [SerializeField] private CinemachineVirtualCamera _camera;
-    [SerializeField] private CharacterStates _characterStates;
-
-    [Header("Jump")] 
-    [SerializeField] private AudioSource _jumpSound;
-    [SerializeField] private ParticleSystem _jumpDustEffect;
-
-    [Header("Dash")]
-    [SerializeField] private AudioSource _dashSound;
-    [SerializeField] private ParticleSystem _dashDustEffect;
-    [SerializeField, Range(0f, 15f)] private float _cameraShakeIntensity;
-    [SerializeField, Range(0, 1f)] private float _cameraShakeTime;
-
-    [SerializeField] private RippleEffect _rippleEffect;
-
-    [Header("Ghost trail")]
-    [SerializeField] private bool _isTrailEnable;
-    [SerializeField, Range(2, 10)] private int _ghostsCount;
-    [SerializeField, Range(0f, 0.3f)] private float _trailDuration;
-
-    [Header("Hurt")]
-    [SerializeField] private AudioSource _hitSound;
+    [Header("Components")] 
+    [SerializeField] private CharacterDash _dash;
     
-    private CharacterMovement _characterMovement;
-    private CharacterDash _characterDash;
-    private CharacterJump _characterJump;
-    private Animator _animator;
+    [Header("VFX")]
+    [SerializeField] private CameraShaker _cameraShaker;
+    [SerializeField] private ParticleSystem _jumpDust;
+    [SerializeField] private ParticleSystem _dashDust;
+    [SerializeField] private DashRipple _dashRipple;
+    [SerializeField] private ShockWave _shockWave;
+    [SerializeField] private CharacterGhostTrail _ghostTrail;
+    [SerializeField] private CharacterOutline _characterOutline;
+    [SerializeField] private Squeeze _jumpSqueeze;
+    
+    [Header("Sounds")] 
+    [SerializeField] private AudioClip _jumpSound;
+    [SerializeField] private AudioClip _dashSound;
+    [SerializeField] private AudioClip _dieSound;
+    
+    private const States Die = States.Die;
+    private const States Jump = States.Jump;
+    private const States Dash = States.Dash;
 
+    private AudioSource _audioSource;
     private Coroutine _shakeCoroutine;
     private Vector3 _characterLastPosition;
-    
-    private void Awake()
+
+    public void Start()
     {
-        _characterJump = GetComponent<CharacterJump>();
-        _characterDash = GetComponent<CharacterDash>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     private void OnEnable()
     {
-        _characterJump.Jumped += OnJumped;
-        _characterStates.StateChanged += HandleState;
+        _dash.CanDashChanged += OnCanDashChanged;
     }
 
     private void OnDisable()
     {
-        _characterJump.Jumped -= OnJumped;
-        _characterStates.StateChanged -= HandleState;
+        _dash.CanDashChanged -= OnCanDashChanged;
     }
 
-    private void HandleState(CharacterStates.States state)
+    public void Play(States state)
     {
-        //_walkSound.Stop();
-        
-        if (state == CharacterStates.States.Jump)
+        switch (state)
         {
-            PlayJumpEffects();
-        }
-        else if (state == CharacterStates.States.Dash)
-        {
-            PlayDashEffect();
-        }
-        else if (state == CharacterStates.States.Move)
-        {
-            //_walkSound.Play();
-        }
-        else if (state == CharacterStates.States.Hurt)
-        {
-            PlayHurtEffect();
+            case Jump:
+                PlayJumpEffects();
+                break;
+            
+            case Dash:
+                PlayDashEffects();
+                break;
+            
+            case Die:
+                PlayHurtEffects();
+                break;
         }
     }
 
     private void PlayJumpEffects()
     {
-        _jumpSound.PlayOneShot(_jumpSound.clip);
-        _jumpDustEffect.Play();
+        _audioSource.PlayOneShot(_jumpSound);
+        _jumpDust.Play();
+        _jumpSqueeze.Play();
     }
 
-    private void PlayDashEffect()
+    private void PlayDashEffects()
     {
-        _dashSound.PlayOneShot(_dashSound.clip);
-        _dashDustEffect.Play(); 
-        //_rippleEffect.Show();
-        
-        StartCoroutine(ShakeCamera());
-        
-        if (_isTrailEnable)
-        {
-            StartCoroutine(ShowGhostTrail());
-        }
-    }
-
-    private void PlayHurtEffect()
-    {
-        _hitSound.PlayOneShot(_hitSound.clip);
-    }
-
-    private void OnJumped()
-    {
-        /*_jumpSound.Play();
-        _jumpDustEffect.Play();*/
-    }
-
-    private void OnDash()
-    {
-        //_dashSound.Play();
-        //_dashDustEffect.Play();
-    }
-
-    private IEnumerator ShakeCamera()//TODO: Need rafactor
-    {
-        CinemachineBasicMultiChannelPerlin _perlin =
-            _camera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-
-        _perlin.m_AmplitudeGain = _cameraShakeIntensity;
-
-        yield return new WaitForSeconds(_cameraShakeTime);
-
-        _perlin.m_AmplitudeGain = 0f;
+        _audioSource.PlayOneShot(_dashSound);
+        _dashDust.Play();
+        _dashRipple.Play(transform.position);
+        _shockWave.Show();
+        _cameraShaker.Shake();
+        _ghostTrail.Show();
     }
     
-    private IEnumerator ShowGhostTrail()//TODO: Make performance tests
+    private void PlayHurtEffects()
     {
-        float delayBetweenGhosts = _trailDuration / (_ghostsCount - 1);
-        
-        CharacterGhostsPool.Instance.GetGhost().Show();
-        
-        for (int i = 0; i < _ghostsCount - 1; i++)
-        {
-            yield return new WaitForSeconds(delayBetweenGhosts);
-            CharacterGhostsPool.Instance.GetGhost().Show();
-        }
+        _audioSource.PlayOneShot(_dieSound);
+    }
+
+    private void OnCanDashChanged(bool canDash)
+    {
+        if (canDash)
+            _characterOutline.Show();
+        else
+            _characterOutline.Hide();
     }
 }
