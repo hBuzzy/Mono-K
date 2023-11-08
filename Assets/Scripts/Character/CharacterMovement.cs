7f0f2c256e7bcae4c338e9c6192734ee929536bf
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-11-08 22:44:38 +0300

    Refactor, improve flip logic

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -1,83 +1,75 @@
-using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class CharacterMovement : MonoBehaviour
 {
-    [Header("Speeds")]
+    [Header("Components")]
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    
+    [Header("Speed settings")]
     [SerializeField, Range(0f, 20f)] private float _maxMoveSpeed;
     [SerializeField, Range(0f, 100f)] private float _turnSpeed;
     [SerializeField, Range(0f, 100f)] private float _airTurnSpeed;
+    [SerializeField, Range(0f, 15f)] private float _friction;
     
-    [Header("Accelerations")]
+    [Header("Acceleration settings")]
     [SerializeField, Range(0f, 100f)] private float _acceleration;
     [SerializeField, Range(0f, 100f)] private float _deceleration;
     [SerializeField, Range(0f, 100f)] private float _airAcceleration;
     [SerializeField, Range(0f, 100f)] private float _airDeceleration;
     
     private PlayerInputActions _playerInput;
-    private Rigidbody2D _rigidbody;
-    private SpriteRenderer _spriteRenderer;
+    private CharacterStateHandler _states;
     private Character _character;
-    private CharacterStates _characterStates;
-
-    private Vector2 _velocity; //TODO: Make it private after tests
-    private Vector2 _desiredVelocity; // TODO: Make it private after tests
-    private Vector2 _platformVelocity;
     
-    private float _speedChange; //TODO: Make it private after tests
-    private float previousDirectionX;
-    private float _movementDirectionX; //TODO: Make it private after tests
+    private Vector2 _desiredVelocity;
+    private Vector2 _platformVelocity;
+    private Vector2 _moveInput;
     
     private bool _isOnPlatform;
+    private bool _isFacingLeft;
 
-    public float MovementDirectionX => _movementDirectionX;
-
+    public Vector2 MoveInput => _moveInput;
+    public bool IsFacingfLeft => _isFacingLeft;
+    
     private void Awake()
     {
         _playerInput = new PlayerInputActions();
-        _characterStates = GetComponent<CharacterStates>();
-        _character = GetComponent<Character>();
     }
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _character = GetComponent<Character>();
+        _states = GetComponent<CharacterStateHandler>();
+
+        _isFacingLeft = false;
     }
 
     private void OnEnable()
     {
         _playerInput.Character.Enable();
     }
-
+    
     private void OnDisable()
     {
         _playerInput.Character.Disable();
     }
-    
+
     private void Update()
     {
-        _movementDirectionX = _playerInput.Character.Move.ReadValue<Vector2>().x;
-            
-        //TODO: If char jump from left wall to  the right but pressing left input char locking at left while must look at right cuz he move in right
-        if (IsDirectionChanged() && _character.IsTouchingWall == false)
-        {
-            FlipDirection();
-            previousDirectionX = _movementDirectionX;
-        }
-
-        float friction = 0;
-        _desiredVelocity = new Vector2(_movementDirectionX, 0f) * Mathf.Max(_maxMoveSpeed - friction, 0f);
+        _moveInput = _playerInput.Character.Move.ReadValue<Vector2>();
+        
+        if (CanFlip())
+            Flip();
+        
+        _desiredVelocity = new Vector2(_moveInput.x, 0f) * Mathf.Max(_maxMoveSpeed - _friction, 0f);
     }
 
     private void FixedUpdate()
     {
-        if (_character.CanMove)
-        {
-            _velocity = _rigidbody.velocity;
-            Move();
-        }
+        if (_character.CanMove == false) 
+            return;
+        
+        Move(_character.Velocity);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -106,54 +98,49 @@ public class CharacterMovement : MonoBehaviour
         }
     }
 
-    private void Move()
+    private void Move(Vector2 velocity)
+    {
+        if (_isOnPlatform)
+            _desiredVelocity += _platformVelocity;
+        
+        velocity.x = Mathf.MoveTowards(velocity.x, _desiredVelocity.x, GetSpeedChange(velocity));
+        
+        _character.SetVelocity(velocity);
+    }
+
+    private float GetSpeedChange(Vector2 velocity)
     {
         bool isGrounded = _character.IsGrounded;
-        
         float acceleration = isGrounded ? _acceleration : _airAcceleration;
         float deceleration = isGrounded ? _deceleration : _airDeceleration;
         float turnSpeed = isGrounded ? _turnSpeed : _airTurnSpeed;
-
-        if (_movementDirectionX == 0) //TODO: Refactoring 
-        {
-            _speedChange = deceleration * Time.deltaTime;
-        }
-        else
-        {
-            if (Mathf.Sign(_movementDirectionX) != Mathf.Sign(_velocity.x))
-            {
-                _speedChange = turnSpeed * Time.deltaTime;
-            }
-            else
-            {
-                _speedChange = acceleration * Time.deltaTime;
-            }
-        }
-
-        if (_isOnPlatform && _movementDirectionX == 0)
-        {
-            _desiredVelocity.x += _platformVelocity.x;
-        }
         
-        _velocity.x = Mathf.MoveTowards(_velocity.x, _desiredVelocity.x, _speedChange);
+        if (_moveInput.x == 0)
+            return deceleration * Time.deltaTime;
 
-        _rigidbody.velocity = _velocity;
-    }
-    
-    private void FlipDirection()//TODO: Change it with facing and velocity?
-    {
-        _spriteRenderer.flipX = _movementDirectionX == (float)MoveDirectionX.Left;
-    }
+        if (Mathf.Sign(_moveInput.x) != Mathf.Sign(velocity.x))
+            return turnSpeed * Time.deltaTime;
 
-    private bool IsDirectionChanged()
-    {
-        return _movementDirectionX != previousDirectionX && _movementDirectionX != (float)MoveDirectionX.Idle;
+        return acceleration * Time.deltaTime;
     }
 
-    private enum MoveDirectionX
+    private void Flip()
     {
-        Left = -1,
-        Idle = 0,
-        Right = 1
+        if (_moveInput.x > 0 && _isFacingLeft)
+        {
+            _isFacingLeft = false;
+            _spriteRenderer.flipX = false;
+        }
+        else if (_moveInput.x < 0 && _isFacingLeft == false)
+        {
+            _isFacingLeft = true;
+            _spriteRenderer.flipX = true;
+        }
+    }
+
+    private bool CanFlip()
+    {
+        return _moveInput.x != 0 && _states.CurrentState is not
+            (CharacterData.States.Slide or CharacterData.States.Grab);
     }
 }
