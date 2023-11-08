fdd38d9bd033cebaaf1edbebd3cc4bcf6c169677
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-11-08 23:03:03 +0300

    Refactor

--- Assets/Scripts/Character/CharacterDeath.cs
+++ Assets/Scripts/Character/CharacterDeath.cs
@@ -2,13 +2,11 @@
 using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Character))]
-
 public class CharacterDeath : MonoBehaviour
 {
-    private Animator _animator;
-    private Coroutine _currentCoroutine;
+    [SerializeField] private EndAnimationNotifier _deathAnimationNotifier;
+    
+    private Coroutine _dieCoroutine;
     private int _deathsCount;
     private bool _deathAnimationFinished;
 
@@ -25,12 +23,22 @@ public class CharacterDeath : MonoBehaviour
     public event Action<bool> DeathChanged;
     public event Action<int> DeathsCountChanged;
 
+    private void OnEnable()
+    {
+        _deathAnimationNotifier.Ended += OnDeathAnimationEnded;
+    }
+
+    private void OnDisable()
+    {
+        _deathAnimationNotifier.Ended -= OnDeathAnimationEnded;
+    }
+
     public void TakeDamage()
     {
-        if (_currentCoroutine != null)
-            StopCoroutine(_currentCoroutine);
+        if (_dieCoroutine != null)
+            StopCoroutine(_dieCoroutine);
         
-        _currentCoroutine = StartCoroutine(Die());
+        _dieCoroutine = StartCoroutine(Die());
     }
 
     private IEnumerator Die()
@@ -41,18 +49,16 @@ public class CharacterDeath : MonoBehaviour
         DeathsCount++;
 
         while (_deathAnimationFinished == false)
-        {
             yield return null;
-        }
 
         yield return CharacterRespawner.Instance.RespawnCharacter();
         
         DeathChanged?.Invoke(false);
 
-        _currentCoroutine = null;
+        _dieCoroutine = null;
     }
 
-    private void OnDeathAnimationFinished()
+    private void OnDeathAnimationEnded()
     {
         _deathAnimationFinished = true;
     }
