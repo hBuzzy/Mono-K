8edcb2f55f96c1b2c785eb6b7c6f83494ff1c438
hBuzzy <47574560+hBuzzy@users.noreply.github.com>
2023-11-08 20:11:58 +0300

    Face and cutscenes fixing

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,79 +1,66 @@
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
-using States = CharacterStates.States;
+using States = CharacterData.States;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CharacterDeath))]
+[RequireComponent(typeof(CharacterMovement))]
 
 public class Character : MonoBehaviour
 {
+    [SerializeField] private Cutscenes _cutscenes;
     [SerializeField] private CharacterGroundDetector _groundDetector;
     [SerializeField] private CharacterWallDetector _wallDetector;
-    [SerializeField] private Transform _respawnPoint; //TODO Make it
-    [SerializeField] private NightSwitcher _nightSwitcher;
-    [SerializeField] private Light2D _light;
-
-    private CharacterStates _states;
-    private PlayerInputActions _playerInput;
+    
+    private CharacterStateHandler _states;
+    private CharacterMovement _movement;
     private Rigidbody2D _rigidbody;
-    private CharacterDash _dashScript; //NeedBeHere?
-    private Outline _outlineScript;
-
-    private States _currentState;
-    
-    private float _inputX;
-    private float _facingDirectionX = 1f;
-    
-    private bool _canMove = true;
-    private bool _isGamePaused;
+    private CharacterDeath _death;
+    private CharacterOutline _characterOutline;
 
     public Vector2 Velocity => _rigidbody.velocity;
+    private States _currentState;
+
+    private bool _canMove = true;
+    private bool _isGamePaused;
+    private bool _isCutsceneActive;
     
-    public float InputX => _inputX;
-    public float FacingDirectionX => _facingDirectionX;
+    public bool IsFacingLeft => _movement.IsFacingfLeft;
+    public float InputX => _movement.MoveInput.x;
     public float GravityScale => _rigidbody.gravityScale;
-    
     public bool CanMove => _canMove;
-    public bool IsTouchingWall => _wallDetector.IsTouchingWall;
+    public bool IsTouchingWall => _wallDetector.IsTouchingWall();
     public bool IsGrounded => _groundDetector.IsGrounded;
 
     private void Awake()
     {
-        _playerInput = new PlayerInputActions();
-        _states = GetComponent<CharacterStates>();
-       // _states = GetComponent<CharacterStateHandler>();
+        _states = GetComponent<CharacterStateHandler>();
+       
         _rigidbody = GetComponent<Rigidbody2D>();
-        _dashScript = GetComponent<CharacterDash>();
+        _death = GetComponent<CharacterDeath>();
+        _movement = GetComponent<CharacterMovement>();
     }
     
     private void OnEnable()
     {
-        _playerInput.Character.Enable();
-        _nightSwitcher.NightStarted += NightStarted;
         _states.StateChanged += OnStateChanged;
+        _death.DeathChanged += OnDeathChanged;
+        _cutscenes.ActiveChanged += OnCutsceneActiveChanged;
 
         GamePause.Instance.PauseChanged += OnGamePauseChanged;
     }
 
     private void OnDisable()
     {
-        _playerInput.Character.Disable();
-        _nightSwitcher.NightStarted -= NightStarted;
         _states.StateChanged -= OnStateChanged;
-    }
-
-    private void Update()
-    {
-        _inputX = _playerInput.Character.Move.ReadValue<Vector2>().x;
-
-        if (_inputX != 0f)
-        {
-            _facingDirectionX = _inputX;
-        }
+        _death.DeathChanged -= OnDeathChanged;
+        _cutscenes.ActiveChanged -= OnCutsceneActiveChanged;  
+ 
+        GamePause.Instance.PauseChanged -= OnGamePauseChanged;
     }
 
     public void SetVelocity(Vector2 velocity)
     {
-        if (_canMove || _currentState == States.Dash)
+        if (_canMove || _currentState == States.Dash || _currentState == States.DashPreparation)
         {
             _rigidbody.velocity = velocity;
         }
@@ -82,47 +69,54 @@ public class Character : MonoBehaviour
     public void SetGravity(float gravityScale)
     {
         if (_canMove)
-        {
             _rigidbody.gravityScale = gravityScale;
-        }
     }
-
-    public void AddForce(Vector2 force)
-    {
-        _rigidbody.AddForce(force, ForceMode2D.Impulse);
-    }
-
 
     public void SetPosition(Vector2 position)
     {
         transform.position = position;
     }
 
-    private void UpdateMoveAbility()
+    public bool IsSlideOnWall(out bool isOnLeft)
     {
-        if (_isGamePaused || _currentState is States.Dash or States.Grab or States.DashPreparation or States.Hurt)
+        if (_wallDetector.IsTouchingWall(out isOnLeft) && IsGrounded == false)
+        {
+            if (_movement.IsFacingfLeft && isOnLeft)
+                return true;
+            
+            if (_movement.IsFacingfLeft == false && isOnLeft == false)
+                return true; 
+
+            return false;
+        }
+        
+        return false;
+    }
+
+    private void UpdateMoveAbility()
+    { 
+        if (_isGamePaused || IsUnmovableState() || _isCutsceneActive)
         {
             _rigidbody.gravityScale = 0f;
-            _rigidbody.velocity = Vector2.zero;
+            
+            if (_states.CurrentState != States.Dash)
+                _rigidbody.velocity = Vector2.zero;
+            
             _canMove = false;
         }
         else
         {
+            _rigidbody.simulated = true;
             _canMove = true;
         }
     }
 
-    private void NightStarted()//TODO: Refactoring
+    private bool IsUnmovableState()
     {
-        Debug.Log("night");
-
-        _light.enabled = true;
-        _dashScript.enabled = true;
-        //_outlineScript.enabled = true;
-        //_trail.enabled = true;
+        return _currentState is States.Dash or States.Grab or States.DashPreparation or States.Die;
     }
 
-    private void OnStateChanged(States state)//TODO:Rename
+    private void OnStateChanged(States state)
     {
         _currentState = state;
         UpdateMoveAbility();
@@ -132,5 +126,15 @@ public class Character : MonoBehaviour
     {
         _isGamePaused = isPaused;
         UpdateMoveAbility();
+    }
+
+    private void OnCutsceneActiveChanged(bool isActive)
+    {
+        _isCutsceneActive = isActive;
+    }
+
+    private void OnDeathChanged(bool isDead)
+    {
+        _rigidbody.simulated = !isDead;
     }
 }
